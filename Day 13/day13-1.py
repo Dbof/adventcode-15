9fee0e81022bbaa88c00eb6086981568f1a79907
Davide Bove <me@davidebove.com>
2015-12-19 18:51:05 +0100

    Added Day 13 solutions

---
+++ Day 13/day13-1.py
@@ -0,0 +1,44 @@
+import itertools
+from collections import defaultdict
+
+dic = defaultdict(dict)
+
+def parse_line_to_dict(dic):
+    # remove whitespace, dot and split the sentence
+    text = line.strip()[:-1].split()
+    # extract information
+    who, gain, amount, nextto = text[0], text[2] == 'gain', int(text[3]), text[-1]
+    if not gain:
+        amount *= -1
+
+    # save to dic
+    dic[who][nextto] = amount
+
+def parse_happyness(dic, keys):
+    result = 0
+    key_len = len(keys)
+    for i in range(0, key_len):
+        k = keys[i]
+        kn = keys[(i+1) % key_len]
+        kprev = keys[i-1]
+
+        ## print(k, "->", kn, '=', dic[k][kn])
+        result += dic[k][kn]
+        ## print(kprev, "->", k, '=', dic[k][kprev])
+        result += dic[k][kprev]
+
+    return result
+
+
+with open('input.txt') as f:
+    for line in f:
+        parse_line_to_dict(dic)
+
+    gen = itertools.permutations(dic.keys())
+
+    max_happy = -10**100
+    for x in gen:
+        happyness = parse_happyness(dic, list(x))
+        max_happy = max(max_happy, happyness)
+    print(max_happy)
+
